c8f8c644d9e9ab24a5292b3a527d99f4c72ca873
Adithya V <99881476+a-dtya@users.noreply.github.com>
2022-11-05 22:45:30 +0530

    functions added

--- main.py
+++ main.py
@@ -21,8 +21,64 @@ async def add(ctx, *arr):
     for i in arr:
         result += int(i)
     await ctx.send(f"Result: {result}")
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title = 'Help',
+        description = 'All bot commands are provided below',
+        color = discord.Color.red()
 
+    )
+    embed.add_field(name='Commands',value='`simplify`,`inversecosine`,`inversesine`,`inversetan`,`absolute`,`log`,`factor`',inline=False)
+    await ctx.send(embed=embed)
 
+@bot.command()
+async def simplify(ctx,a):
+    val = urllib.parse.quote_plus(a)
+    url = "https://newton.now.sh/api/v2/simplify/"+val
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")
+@bot.command()
+async def inversecosine(ctx,a):
+    url = "https://newton.now.sh/api/v2/arccos/"+a
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")
 
-bot.run(secret.BOT_TOKEN)    
+@bot.command()
+async def inversesine(ctx,a):
+    url = "https://newton.now.sh/api/v2/arcsin/"+a
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}") 
+@bot.command()
+async def inversetan(ctx,a):
+    url = "https://newton.now.sh/api/v2/arctan/"+a
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")      
+@bot.command()
+async def absolute(ctx,a):
+    url = "https://newton.now.sh/api/v2/abs/"+a
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")
+@bot.command()
+async def log(ctx,a):
+    url = "https://newton.now.sh/api/v2/log/"+a
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")      
+@bot.command()
+async def factor(ctx,a):
+    val = urllib.parse.quote_plus(a)
+    url = "https://newton.now.sh/api/v2/factor/"+val
+    response = requests.get(url)
+    get_json = json.loads(response.text)
+    await ctx.send(f"Result:{get_json['result']}")  
+
+bot.run(secret.BOT_TOKEN)
+    
+     
 
